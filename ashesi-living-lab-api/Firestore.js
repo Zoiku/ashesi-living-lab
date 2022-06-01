398d38ea091dbd828bdcb1fc7f452573999ed143
zoiku <j.zoiku@gmail.com>
2022-06-01 14:52:21 +0000

    update

--- ashesi-living-lab-api/Firestore.js
+++ ashesi-living-lab-api/Firestore.js
@@ -1,35 +1,35 @@
-import {getDatabase, ref, child, get, set} from "firebase/database"
-import {initializeApp} from "firebase/app"
+import { getDatabase, ref, child, get, set } from "firebase/database";
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAnkrwbbw6dKlE70-zLbFrhAqiLwBVhQM4",
-    authDomain: "ashesi-living-lab.firebaseapp.com",
-    databaseURL: "https://ashesi-living-lab-default-rtdb.firebaseio.com",
-    projectId: "ashesi-living-lab",
-    storageBucket: "ashesi-living-lab.appspot.com",
-    messagingSenderId: "1017561467972",
-    appId: "1:1017561467972:web:c26f97c75f49c35bda4011",
-    measurementId: "G-NPQQ0DMN64"
-}
+  apiKey: "AIzaSyAneKmtFgBTlF-Hv8kSuDayJP1pG3usigs",
+  authDomain: "ashesi-as-a-living-lab-af663.firebaseapp.com",
+  databaseURL:
+    "https://ashesi-as-a-living-lab-af663-default-rtdb.firebaseio.com",
+  projectId: "ashesi-as-a-living-lab-af663",
+  storageBucket: "ashesi-as-a-living-lab-af663.appspot.com",
+  messagingSenderId: "323009906773",
+  appId: "1:323009906773:web:db6b1d42f6639e192bacdc",
+};
 
 class FIRESTOREDATABASE {
-    database = null;
-    constructor() {
-        this.database = getDatabase(initializeApp(firebaseConfig))
-    }
+  database = null;
+  constructor() {
+    this.database = getDatabase(initializeApp(firebaseConfig));
+  }
 
-    getData() {
-        const dbRef = ref(this.database);
-        return get(child(dbRef, `Lecture-Hall/Lab222`))
-    }
+  getData() {
+    const dbRef = ref(this.database);
+    return get(child(dbRef, `Lecture-Hall/Lab222`));
+  }
 
-    postData(id,humidity,motion,light) {
-        return set(ref(this.database, `Lecture-Hall/Lab222/${id}` ), {
-            humidity: humidity,
-            motion: motion,
-            light: light
-        });
-    }
+  postData(id, humidity, motion, light) {
+    return set(ref(this.database, `Lecture-Hall/Lab222/${id}`), {
+      humidity: humidity,
+      motion: motion,
+      light: light,
+    });
+  }
 }
 
-export default FIRESTOREDATABASE
+export default FIRESTOREDATABASE;
--- ashesi-living-lab-api/index.js
+++ ashesi-living-lab-api/index.js
@@ -1,31 +1,58 @@
-import cors from 'cors'
-import express from 'express'
-import FIRESTOREDATABASE from './Firestore.js'
+import cors from "cors";
+import express from "express";
+import FIRESTOREDATABASE from "./Firestore.js";
 
-const Database = new FIRESTOREDATABASE()
-const app = express()
-const port = process.env.PORT || 3000
+const Database = new FIRESTOREDATABASE();
+const app = express();
+const port = process.env.PORT || 3000;
 const corsOptions = {
-    origin: "*",
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.listen(port, ()=>console.log('Listening'))
+app.listen(port);
 
-app.get('/api/get/', async(_req,res)=>{
-    const data = await Database.getData()
-    res.json(data.val())
-})
+app.get("/api/get/", async (_req, res) => {
+  const data = await Database.getData();
+  res.json(data.val());
+});
 
-app.post('/api/post/', (req,res)=>{
-    const request = {
-        id: req.body.id,
-        humidity: req.body.humidity,
-        motion: req.body.motion,
-        light: req.body.light
-    }
-    Database.postData(request.id,request.humidity,request.motion,request.light)    
-    res.send('OK')
-})
+app.post("/api/post/", (req, res) => {
+  const request = {
+    id: req.body.id,
+    humidity: req.body.humidity,
+    motion: req.body.motion,
+    light: req.body.light,
+  };
+  Database.postData(
+    request.id,
+    request.humidity,
+    request.motion,
+    request.light
+  );
+  res.send("OK");
+});
+
+// const mqttSimulation = () => {
+//   console.log("Starting simulation...");
+
+//   let index = 0;
+
+//   setInterval(() => {
+//     Database.postData(
+//       index,
+//       Math.floor(Math.random() * 101),
+//       Math.floor(Math.random() * 101),
+//       Math.floor(Math.random() * 101)
+//     );
+
+//   if(index === 24) {
+//     index = 0
+//   } else {
+//     index += 1;
+//   }
+  
+//   }, 1000);
+// };

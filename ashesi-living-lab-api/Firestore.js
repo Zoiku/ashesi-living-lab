85d8aeeb445c60b08b16d9fc5e26c53016b1bebc
Mark Zoiku <59940634+Zoiku@users.noreply.github.com>
2025-06-28 23:24:50 +0000

    Update Firestore.js

--- ashesi-living-lab-api/Firestore.js
+++ ashesi-living-lab-api/Firestore.js
@@ -2,14 +2,13 @@ import { getDatabase, ref, child, get, set } from "firebase/database";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAneKmtFgBTlF-Hv8kSuDayJP1pG3usigs",
-  authDomain: "ashesi-as-a-living-lab-af663.firebaseapp.com",
-  databaseURL:
-    "https://ashesi-as-a-living-lab-af663-default-rtdb.firebaseio.com",
-  projectId: "ashesi-as-a-living-lab-af663",
-  storageBucket: "ashesi-as-a-living-lab-af663.appspot.com",
-  messagingSenderId: "323009906773",
-  appId: "1:323009906773:web:db6b1d42f6639e192bacdc",
+  apiKey: XXX,
+  authDomain: XXX,
+  databaseURL: XXX,
+  projectId: XXX,
+  storageBucket: XXX,
+  messagingSenderId: XXX,
+  appId: XXX,
 };
 
 class FIRESTOREDATABASE {
